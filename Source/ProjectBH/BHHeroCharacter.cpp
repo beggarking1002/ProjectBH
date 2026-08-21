4c5e12315209240da6ed531e82fd0d97015baa60
beggarking1002 <drake1002@naver.com>
2026-08-21 10:03:33 +0900

    카메라 세팅
    카메라 세팅

--- Source/ProjectBH/BHHeroCharacter.cpp
+++ Source/ProjectBH/BHHeroCharacter.cpp
@@ -2,7 +2,32 @@
 
 #include "BHHeroCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 ABHHeroCharacter::ABHHeroCharacter()
 {
 	AutoPossessAI = EAutoPossessAI::Disabled;
+	GetCapsuleComponent()->InitCapsuleSize(42.0f, 96.0f);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
+	GetCharacterMovement()->MaxWalkSpeed = 400.0f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.0f;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 200.0f;
+	CameraBoom->SocketOffset = FVector(0.0f, 55.0f, 65.0f);
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
 }
--- Source/ProjectBH/BHHeroCharacter.h
+++ Source/ProjectBH/BHHeroCharacter.h
@@ -6,6 +6,9 @@
 #include "BHBaseCharacter.h"
 #include "BHHeroCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
 /** Player-controlled character foundation for the first combat slice. */
 UCLASS()
 class PROJECTBH_API ABHHeroCharacter : public ABHBaseCharacter
@@ -14,4 +17,15 @@ class PROJECTBH_API ABHHeroCharacter : public ABHBaseCharacter
 
 public:
 	ABHHeroCharacter();
+
+	UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+protected:
+	/** Positions the camera behind the character and handles collision with level geometry. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USpringArmComponent> CameraBoom;
+
+	/** Player view camera attached to the end of CameraBoom. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCameraComponent> FollowCamera;
 };
